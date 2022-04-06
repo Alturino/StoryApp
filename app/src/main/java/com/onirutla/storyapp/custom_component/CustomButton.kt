d0bfcd41174fccb8e9b9c2d9afe718af311b9b8b
onirutlA <alturino001@gmail.com>
2022-04-06 18:00:32 +0700

    add: CustomButton

---
+++ app/src/main/java/com/onirutla/storyapp/custom_component/CustomButton.kt
@@ -0,0 +1,45 @@
+package com.onirutla.storyapp.custom_component
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+import com.onirutla.storyapp.R
+
+class CustomButton : AppCompatButton {
+
+    private lateinit var enabledBackground: Drawable
+    private lateinit var disabledBackground: Drawable
+    private var txtColor: Int = 0
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defAttrs: Int) : super(
+        context,
+        attrs,
+        defAttrs
+    ) {
+        init()
+    }
+
+    private fun init() {
+        txtColor = ContextCompat.getColor(context, R.color.purple_500)
+        enabledBackground = ContextCompat.getDrawable(context, R.drawable.bg_button) as Drawable
+        disabledBackground =
+            ContextCompat.getDrawable(context, R.drawable.bg_button_disable) as Drawable
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        background = if (isEnabled) enabledBackground else disabledBackground
+        text = if (isEnabled) "Submit" else "Please Kindly Insert Form"
+    }
+}
