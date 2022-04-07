b76c820910c6cf24c224fcca1ac6a34abdada135
onirutlA <alturino001@gmail.com>
2022-04-07 16:34:52 +0700

    add: util function to compressImage

--- app/src/main/java/com/onirutla/storyapp/util/Util.kt
+++ app/src/main/java/com/onirutla/storyapp/util/Util.kt
@@ -1,5 +1,26 @@
 package com.onirutla.storyapp.util
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.Patterns
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
-fun String.isValidEmail() = Patterns.EMAIL_ADDRESS.matcher(this).matches()
+fun String.isValidEmail(): Boolean = Patterns.EMAIL_ADDRESS.matcher(this).matches()
+
+fun File.compressImage(): File {
+    val bitmap = BitmapFactory.decodeFile(path)
+    var streamLength: Int
+    var compressQuality = 100
+    do {
+        val bmpStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, compressQuality, bmpStream)
+        val bmpPicByteArray = bmpStream.toByteArray()
+        streamLength = bmpPicByteArray.size
+        compressQuality -= 5
+    } while (streamLength > 1000000)
+
+    bitmap.compress(Bitmap.CompressFormat.JPEG, compressQuality, FileOutputStream(this))
+    return this
+}
