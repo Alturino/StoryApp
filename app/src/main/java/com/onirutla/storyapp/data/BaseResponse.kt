8b4b9c129b6f4379e06662a104889842522ae5ae
onirutlA <alturino001@gmail.com>
2022-04-07 09:58:37 +0700

    add: BaseResponse.kt

---
+++ app/src/main/java/com/onirutla/storyapp/data/BaseResponse.kt
@@ -0,0 +1,12 @@
+package com.onirutla.storyapp.data
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class BaseResponse(
+    @Json(name = "error")
+    val error: Boolean? = false,
+    @Json(name = "message")
+    val message: String? = ""
+)
