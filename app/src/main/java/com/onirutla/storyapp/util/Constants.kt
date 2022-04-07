c24516ee2e398d8f056fd92b58cf06f4d0cc5f05
onirutlA <alturino001@gmail.com>
2022-04-07 22:39:10 +0700

    add: constant build url

--- app/src/main/java/com/onirutla/storyapp/util/Constants.kt
+++ app/src/main/java/com/onirutla/storyapp/util/Constants.kt
@@ -1,6 +1,9 @@
 package com.onirutla.storyapp.util
 
+import com.onirutla.storyapp.BuildConfig
+
 object Constants {
     const val STORY_API_STARTING_INDEX = 1
     const val NETWORK_LOAD_SIZE = 20
+    const val BASE_URL = BuildConfig.BASE_URL
 }
