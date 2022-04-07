59b1f512af68de77b39f04544791b1597597335a
onirutlA <alturino001@gmail.com>
2022-04-07 09:59:47 +0700

    add: UserResponse.kt

---
+++ app/src/main/java/com/onirutla/storyapp/data/user/response/UserResponse.kt
@@ -0,0 +1,14 @@
+package com.onirutla.storyapp.data.user.response
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class UserResponse(
+    @Json(name = "error")
+    val error: Boolean? = false,
+    @Json(name = "loginResult")
+    val loginResponse: LoginResponse? = LoginResponse(),
+    @Json(name = "message")
+    val message: String? = ""
+)
