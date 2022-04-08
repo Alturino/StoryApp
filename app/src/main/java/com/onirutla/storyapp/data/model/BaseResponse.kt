aa894e8a4d4123a806cc0a9c01c8481480424ef0
onirutlA <alturino001@gmail.com>
2022-04-09 05:50:06 +0700

    add: Parcelize in BaseResponse.kt

--- app/src/main/java/com/onirutla/storyapp/data/model/BaseResponse.kt
+++ app/src/main/java/com/onirutla/storyapp/data/model/BaseResponse.kt
@@ -1,12 +1,15 @@
 package com.onirutla.storyapp.data.model
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import kotlinx.parcelize.Parcelize
 
 @JsonClass(generateAdapter = true)
+@Parcelize
 data class BaseResponse(
     @Json(name = "error")
     val error: Boolean? = false,
     @Json(name = "message")
     val message: String? = ""
-)
+): Parcelable
