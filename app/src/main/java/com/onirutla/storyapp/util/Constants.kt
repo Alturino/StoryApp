c9a9978b33b0da8e4fe187ba033e37542786b37c
onirutlA <alturino001@gmail.com>
2022-04-08 10:58:00 +0700

    add: Constant LOGIN_DATASTORE and LOGIN_TOKEN

--- app/src/main/java/com/onirutla/storyapp/util/Constants.kt
+++ app/src/main/java/com/onirutla/storyapp/util/Constants.kt
@@ -6,4 +6,6 @@ object Constants {
     const val STORY_API_STARTING_INDEX = 1
     const val NETWORK_LOAD_SIZE = 20
     const val BASE_URL = BuildConfig.BASE_URL
+    const val LOGIN_DATASTORE = BuildConfig.LOGIN_DATASTORE
+    const val LOGIN_TOKEN = BuildConfig.LOGIN_TOKEN
 }
