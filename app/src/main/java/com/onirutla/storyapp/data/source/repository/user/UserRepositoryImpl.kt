95b3681f5d1ac6ad70222ec2c929c12729cb1e50
onirutlA <alturino001@gmail.com>
2022-04-08 14:06:57 +0700

    add: private fun to addTokenToPreferences if response isSuccessful

--- app/src/main/java/com/onirutla/storyapp/data/source/repository/user/UserRepositoryImpl.kt
+++ app/src/main/java/com/onirutla/storyapp/data/source/repository/user/UserRepositoryImpl.kt
@@ -5,12 +5,19 @@ import com.onirutla.storyapp.data.model.BaseResponse
 import com.onirutla.storyapp.data.model.user.body.UserLoginBody
 import com.onirutla.storyapp.data.model.user.body.UserRegisterBody
 import com.onirutla.storyapp.data.model.user.response.LoginResponse
+import com.onirutla.storyapp.data.source.DataStoreManager
 import com.onirutla.storyapp.data.source.api_service.UserApiService
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
-    private val userApiService: UserApiService
+    private val userApiService: UserApiService,
+    private val dataStoreManager: DataStoreManager
 ) : UserRepository {
+
+    private suspend fun addTokenToPreferences(loginToken: String) {
+        dataStoreManager.updateLoginToken(loginToken)
+    }
 
     override suspend fun registerUser(registerBody: UserRegisterBody): BaseResponse =
         try {
@@ -31,6 +38,7 @@ class UserRepositoryImpl @Inject constructor(
         val response = userApiService.loginUser(loginBody)
         if (response.isSuccessful) {
             Log.d("userRepository", "${response.body()}")
+            response.body()?.loginData?.token?.let { addTokenToPreferences(it) }
             response.body()!!
         } else {
             Log.d("userRepository", "${response.errorBody()}")
@@ -40,4 +48,6 @@ class UserRepositoryImpl @Inject constructor(
         Log.d("userRepository", "$e", e)
         LoginResponse()
     }
+
+    override val userToken: Flow<String> get() = dataStoreManager.preferenceLoginToken
 }
