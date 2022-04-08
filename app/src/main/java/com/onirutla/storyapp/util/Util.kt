f311a3b63fdb8ed97ea5fb9e0196737a6a3c6990
onirutlA <alturino001@gmail.com>
2022-04-09 05:54:33 +0700

    add: util function

--- app/src/main/java/com/onirutla/storyapp/util/Util.kt
+++ app/src/main/java/com/onirutla/storyapp/util/Util.kt
@@ -1,14 +1,23 @@
 package com.onirutla.storyapp.util
 
+import android.content.ContentResolver
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Environment
 import android.util.Patterns
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.onirutla.storyapp.util.Constants.FILENAME_FORMAT
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
 fun String.isValidEmail(): Boolean = Patterns.EMAIL_ADDRESS.matcher(this).matches()
 
@@ -31,4 +40,29 @@ fun File.compressImage(): File {
 @BindingAdapter("load_image")
 fun loadImage(view: ImageView, any: Any?) {
     any?.let { Glide.with(view).load(it).into(view) }
+}
+
+val timeStamp: String = SimpleDateFormat(
+    FILENAME_FORMAT,
+    Locale.US
+).format(System.currentTimeMillis())
+
+fun Context.createTempFile(): File {
+    val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    return File.createTempFile(timeStamp, ".jpg", storageDir)
+}
+
+fun Uri.uriToFile(context: Context): File {
+    val contentResolver: ContentResolver = context.contentResolver
+    val myFile = context.createTempFile()
+
+    val inputStream = contentResolver.openInputStream(this) as InputStream
+    val outputStream: OutputStream = FileOutputStream(myFile)
+    val buf = ByteArray(1024)
+    var len: Int
+    while (inputStream.read(buf).also { len = it } > 0) outputStream.write(buf, 0, len)
+    outputStream.close()
+    inputStream.close()
+
+    return myFile
 }
