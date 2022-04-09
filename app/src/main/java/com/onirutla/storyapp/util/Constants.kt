2b8050e151e4c9b218a437c8eabe697d42ca63f3
onirutlA <alturino001@gmail.com>
2022-04-09 15:00:56 +0700

    add: Constant REGISTER_RESPONSE and ADD_STORY_RESPONSE

--- app/src/main/java/com/onirutla/storyapp/util/Constants.kt
+++ app/src/main/java/com/onirutla/storyapp/util/Constants.kt
@@ -9,4 +9,6 @@ object Constants {
     const val LOGIN_DATASTORE = BuildConfig.LOGIN_DATASTORE
     const val LOGIN_TOKEN = BuildConfig.LOGIN_TOKEN
     const val FILENAME_FORMAT = "dd-MMM-yyyy"
+    const val REGISTER_RESPONSE = "register_response"
+    const val ADD_STORY_RESPONSE = "add_story_response"
 }
