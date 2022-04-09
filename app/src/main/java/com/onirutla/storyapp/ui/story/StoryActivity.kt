2d6e617011b0e97d47762ea3048f50577d454f82
onirutlA <alturino001@gmail.com>
2022-04-09 22:04:15 +0700

    add: UI logic in logout

--- app/src/main/java/com/onirutla/storyapp/ui/story/StoryActivity.kt
+++ app/src/main/java/com/onirutla/storyapp/ui/story/StoryActivity.kt
@@ -2,6 +2,8 @@ package com.onirutla.storyapp.ui.story
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -9,9 +11,11 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.onirutla.storyapp.R
 import com.onirutla.storyapp.data.model.BaseResponse
 import com.onirutla.storyapp.databinding.ActivityStoryBinding
 import com.onirutla.storyapp.ui.add_story.AddStoryActivity
+import com.onirutla.storyapp.ui.login.LoginActivity
 import com.onirutla.storyapp.util.Constants.ADD_STORY_RESPONSE
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -61,6 +65,30 @@ class StoryActivity : AppCompatActivity() {
             layoutManager = LinearLayoutManager(this@StoryActivity)
             setHasFixedSize(true)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.logout -> {
+                viewModel.logout()
+                startActivity(
+                    Intent(
+                        this,
+                        LoginActivity::class.java
+                    ).apply {
+                        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                    }
+                )
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun startAddActivity() {
