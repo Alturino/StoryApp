d33f9490ed2bd594e1caaffbf1b9f9488d2f020e
onirutlA <alturino001@gmail.com>
2022-04-10 09:26:56 +0700

    refactor: chage base_url, login_token, login_datastore to constant

--- app/src/main/java/com/onirutla/storyapp/util/Constants.kt
+++ app/src/main/java/com/onirutla/storyapp/util/Constants.kt
@@ -1,13 +1,11 @@
 package com.onirutla.storyapp.util
 
-import com.onirutla.storyapp.BuildConfig
-
 object Constants {
     const val STORY_API_STARTING_INDEX = 1
     const val NETWORK_LOAD_SIZE = 20
-    const val BASE_URL = BuildConfig.BASE_URL
-    const val LOGIN_DATASTORE = BuildConfig.LOGIN_DATASTORE
-    const val LOGIN_TOKEN = BuildConfig.LOGIN_TOKEN
+    const val BASE_URL = "https://story-api.dicoding.dev/v1/"
+    const val LOGIN_TOKEN = "login_token"
+    const val LOGIN_DATASTORE = "login_datastore"
     const val FILENAME_FORMAT = "dd-MMM-yyyy"
     const val REGISTER_RESPONSE = "register_response"
     const val ADD_STORY_RESPONSE = "add_story_response"
