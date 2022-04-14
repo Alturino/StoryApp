52335f9d0dbfd30f70e83c16079a56634c29b727
onirutlA <alturino001@gmail.com>
2022-04-14 13:43:30 +0700

    add: DataDummy.kt

---
+++ app/src/test/java/com/onirutla/storyapp/data/DataDummy.kt
@@ -0,0 +1,15 @@
+package com.onirutla.storyapp.data
+
+import com.onirutla.storyapp.data.model.story.StoryResponse
+
+object DataDummy {
+    fun generateStories(): List<StoryResponse> {
+        val stories = mutableListOf<StoryResponse>()
+
+        repeat(30) {
+            stories.add(StoryResponse(id = "$it", name = "Story $it"))
+        }
+
+        return stories
+    }
+}
