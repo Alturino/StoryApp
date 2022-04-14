a313af4222c5b307d0a15de369465430a89219f9
onirutlA <alturino001@gmail.com>
2022-04-14 21:01:27 +0700

    refactor: change size default value to NETWORK_LOAD_SIZE

--- app/src/main/java/com/onirutla/storyapp/data/source/api_service/StoryApiService.kt
+++ app/src/main/java/com/onirutla/storyapp/data/source/api_service/StoryApiService.kt
@@ -3,15 +3,13 @@ package com.onirutla.storyapp.data.source.api_service
 import com.onirutla.storyapp.data.model.BaseResponse
 import com.onirutla.storyapp.data.model.PageResponse
 import com.onirutla.storyapp.data.model.story.StoryResponse
+import com.onirutla.storyapp.util.Constants.NETWORK_LOAD_SIZE
 import okhttp3.MultipartBody
-import okhttp3.RequestBody
-import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
-import retrofit2.http.PartMap
 import retrofit2.http.Query
 
 interface StoryApiService {
@@ -19,7 +17,7 @@ interface StoryApiService {
     @GET(value = "stories")
     suspend fun getAllStoriesWithToken(
         @Query("page") page: Int = 1,
-        @Query("size") size: Int = 1,
+        @Query("size") size: Int = NETWORK_LOAD_SIZE,
         @Header("Authorization") token: String
     ): PageResponse<StoryResponse>
 
