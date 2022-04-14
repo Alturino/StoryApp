01f4f1781c888d12b012401dd3bfea5435b6bfcb
onirutlA <alturino001@gmail.com>
2022-04-14 19:43:04 +0700

    add: change implementation of addNewStoryViewModel

--- app/src/main/java/com/onirutla/storyapp/ui/add_story/AddStoryViewModel.kt
+++ app/src/main/java/com/onirutla/storyapp/ui/add_story/AddStoryViewModel.kt
@@ -7,26 +7,20 @@ import androidx.lifecycle.viewModelScope
 import com.onirutla.storyapp.data.model.BaseResponse
 import com.onirutla.storyapp.data.source.repository.story.StoryRepository
 import com.onirutla.storyapp.data.source.repository.user.UserRepository
-import com.onirutla.storyapp.util.Util.compressImage
+import com.onirutla.storyapp.util.Util.toMultipart
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.MultipartBody
-import okhttp3.RequestBody.Companion.asRequestBody
 import java.io.File
 import javax.inject.Inject
 
 @HiltViewModel
 class AddStoryViewModel @Inject constructor(
     private val storyRepository: StoryRepository,
-    userRepository: UserRepository
+    private val userRepository: UserRepository
 ) : ViewModel() {
 
     private val _image = MutableLiveData<File>()
     val image: LiveData<File> get() = _image
-
-    private val token = userRepository.userToken
 
     private val _response = MutableLiveData<BaseResponse>()
     val addStoryResponse: LiveData<BaseResponse> get() = _response
@@ -39,22 +33,16 @@ class AddStoryViewModel @Inject constructor(
 
     fun addNewStoryWithToken(description: String, image: File) {
         viewModelScope.launch {
-            val compressedImage = image.compressImage()
-            val imageRequestBody = compressedImage.asRequestBody("image/jpeg".toMediaTypeOrNull())
-            val imageMultiPart = MultipartBody.Part.createFormData(
-                "photo",
-                compressedImage.name,
-                imageRequestBody
+            val imageMultiPart = image.toMultipart()
+            val descriptionMultipart = description.toMultipart()
+            val token = userRepository.getUserToken()
+            _response.value = storyRepository.addNewStoryWithToken(
+                descriptionMultipart,
+                imageMultiPart,
+                "Bearer $token"
             )
-            val descriptionMultipart = MultipartBody.Part.createFormData("description", description)
-            token.collect {
-                _response.value = storyRepository.addNewStoryWithToken(
-                    descriptionMultipart,
-                    imageMultiPart,
-                    "Bearer $token"
-                )
-            }
         }
+
     }
 
 }
