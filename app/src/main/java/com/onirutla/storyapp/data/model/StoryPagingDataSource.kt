36ee957c4c6f1005d2516ae3a5dc77e8b0c732e1
onirutlA <alturino001@gmail.com>
2022-04-15 22:00:41 +0700

    add: wrapEspressoIdlingResource to PagingDataSource

--- app/src/main/java/com/onirutla/storyapp/data/model/StoryPagingDataSource.kt
+++ app/src/main/java/com/onirutla/storyapp/data/model/StoryPagingDataSource.kt
@@ -6,6 +6,7 @@ import com.onirutla.storyapp.data.model.story.StoryResponse
 import com.onirutla.storyapp.data.source.api_service.StoryApiService
 import com.onirutla.storyapp.util.Constants.NETWORK_LOAD_SIZE
 import com.onirutla.storyapp.util.Constants.STORY_API_STARTING_INDEX
+import com.onirutla.storyapp.util.wrapEspressoIdlingResource
 import okio.IOException
 import retrofit2.HttpException
 
@@ -20,23 +21,26 @@ class StoryPagingDataSource(
                 ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
         }
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, StoryResponse> = try {
-        val position = params.key ?: STORY_API_STARTING_INDEX
-        val response = apiService.getAllStoriesWithToken(
-            page = position,
-            size = NETWORK_LOAD_SIZE,
-            token = token
-        )
-        val stories = response.listStory
-        val nextKey = if (stories.isNullOrEmpty()) null else position + 1
-        LoadResult.Page(
-            data = stories.orEmpty(),
-            prevKey = if (position == STORY_API_STARTING_INDEX) null else position - 1,
-            nextKey = nextKey
-        )
-    } catch (exception: IOException) {
-        LoadResult.Error(exception)
-    } catch (exception: HttpException) {
-        LoadResult.Error(exception)
-    }
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, StoryResponse> =
+        wrapEspressoIdlingResource {
+            try {
+                val position = params.key ?: STORY_API_STARTING_INDEX
+                val response = apiService.getAllStoriesWithToken(
+                    page = position,
+                    size = NETWORK_LOAD_SIZE,
+                    token = token
+                )
+                val stories = response.listStory
+                val nextKey = if (stories.isNullOrEmpty()) null else position + 1
+                LoadResult.Page(
+                    data = stories.orEmpty(),
+                    prevKey = if (position == STORY_API_STARTING_INDEX) null else position - 1,
+                    nextKey = nextKey
+                )
+            } catch (exception: IOException) {
+                LoadResult.Error(exception)
+            } catch (exception: HttpException) {
+                LoadResult.Error(exception)
+            }
+        }
 }
