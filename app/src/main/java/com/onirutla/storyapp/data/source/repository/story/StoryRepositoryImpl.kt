ab45f31026446395e06f82d2d692a63f811d7ba1
onirutlA <alturino001@gmail.com>
2022-04-15 22:06:04 +0700

    add: wrapEspressoIdlingResource to StoryViewModel.kt

--- app/src/main/java/com/onirutla/storyapp/data/source/repository/story/StoryRepositoryImpl.kt
+++ app/src/main/java/com/onirutla/storyapp/data/source/repository/story/StoryRepositoryImpl.kt
@@ -1,7 +1,5 @@
 package com.onirutla.storyapp.data.source.repository.story
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.liveData
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
@@ -11,7 +9,6 @@ import com.onirutla.storyapp.data.model.StoryPagingDataSource
 import com.onirutla.storyapp.data.model.story.StoryResponse
 import com.onirutla.storyapp.data.source.api_service.StoryApiService
 import com.onirutla.storyapp.util.Constants.NETWORK_LOAD_SIZE
-import com.onirutla.storyapp.util.EspressoIdlingResource
 import com.onirutla.storyapp.util.wrapEspressoIdlingResource
 import kotlinx.coroutines.flow.Flow
 import okhttp3.MultipartBody
@@ -25,13 +22,13 @@ class StoryRepositoryImpl @Inject constructor(
         description: MultipartBody.Part,
         image: MultipartBody.Part,
         token: String
-    ): BaseResponse = try {
-        EspressoIdlingResource.increment()
-        val response = storyApiService.addNewStoryWithToken(description, image, token)
-        EspressoIdlingResource.decrement()
-        response
-    } catch (e: Exception) {
-        BaseResponse(error = true, e.localizedMessage)
+    ): BaseResponse = wrapEspressoIdlingResource {
+        try {
+            val response = storyApiService.addNewStoryWithToken(description, image, token)
+            response
+        } catch (e: Exception) {
+            BaseResponse(error = true, e.localizedMessage)
+        }
     }
 
     override suspend fun getStoriesWithTokenAndLocation(token: String): PageResponse<StoryResponse> =
