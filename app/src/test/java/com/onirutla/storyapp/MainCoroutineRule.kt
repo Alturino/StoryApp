003402c9cee56ff18d2b44c12f873cb3d55b7153
onirutlA <alturino001@gmail.com>
2022-04-15 11:37:19 +0700

    refactor: MainCoroutineRule.kt

--- app/src/test/java/com/onirutla/storyapp/MainCoroutineRule.kt
+++ app/src/test/java/com/onirutla/storyapp/MainCoroutineRule.kt
@@ -3,7 +3,9 @@ package com.onirutla.storyapp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineExceptionHandler
 import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.createTestCoroutineScope
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
@@ -11,9 +13,9 @@ import org.junit.runner.Description
 
 @ExperimentalCoroutinesApi
 class MainCoroutineRule(
-    val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+    private val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
 ) : TestWatcher(),
-    TestCoroutineScope by TestCoroutineScope(dispatcher) {
+    TestCoroutineScope by createTestCoroutineScope(TestCoroutineDispatcher() + TestCoroutineExceptionHandler() + dispatcher) {
     override fun starting(description: Description?) {
         super.starting(description)
         Dispatchers.setMain(dispatcher)
