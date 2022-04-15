e76319a88269b5a43c25e9bb2cdb4ee8ccf64c49
onirutlA <alturino001@gmail.com>
2022-04-15 14:48:49 +0700

    add: more test to StoryRepositoryTest.kt

--- app/src/test/java/com/onirutla/storyapp/data/source/repository/story/StoryRepositoryTest.kt
+++ app/src/test/java/com/onirutla/storyapp/data/source/repository/story/StoryRepositoryTest.kt
@@ -1,6 +1,8 @@
 package com.onirutla.storyapp.data.source.repository.story
 
 import com.onirutla.storyapp.data.model.BaseResponse
+import com.onirutla.storyapp.data.model.PageResponse
+import com.onirutla.storyapp.data.model.story.StoryResponse
 import com.onirutla.storyapp.data.source.api_service.StoryApiService
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
@@ -23,6 +25,18 @@ class StoryRepositoryTest {
     private lateinit var storyApiService: StoryApiService
 
     private val token = "Bearer token"
+    private val image = File("screenshot.png")
+    private val description = "description"
+    private val imageRequestBody = image.asRequestBody("image/jpeg".toMediaTypeOrNull())
+    private val descriptionMultipart = MultipartBody.Part.createFormData(
+        "description",
+        description
+    )
+    private val imageMultiPart = MultipartBody.Part.createFormData(
+        "photo",
+        image.name,
+        imageRequestBody
+    )
 
 
     @Before
@@ -35,25 +49,13 @@ class StoryRepositoryTest {
     fun `given token getAllStoriesWithToken should not return null`() = runBlockingTest {
         val actual = storyRepository.getAllStoriesWithToken(token)
 
-
         assertNotNull(actual)
     }
 
     @Test
     fun `given imageMultiPart, descriptionMultiPart, token addNewStoryWithoutToken should return BaseResponse(error=false)`() =
         runBlockingTest {
-            val description = "description"
-            val image = File("image")
-            val imageRequestBody = image.asRequestBody("image/jpeg".toMediaTypeOrNull())
-            val descriptionMultipart = MultipartBody.Part.createFormData(
-                "description",
-                description
-            )
-            val imageMultiPart = MultipartBody.Part.createFormData(
-                "photo",
-                image.name,
-                imageRequestBody
-            )
+
             `when`(
                 storyApiService.addNewStoryWithToken(
                     descriptionMultipart,
@@ -75,5 +77,70 @@ class StoryRepositoryTest {
                 imageMultiPart,
                 token
             )
+        }
+
+    @Test
+    fun `given exception addNewStoryWithToken should return BaseResponse(error=true)`() =
+        runBlockingTest {
+            `when`(
+                storyApiService.addNewStoryWithToken(
+                    descriptionMultipart,
+                    imageMultiPart,
+                    token
+                )
+            ).thenThrow(IllegalArgumentException())
+
+            val actual =
+                storyRepository.addNewStoryWithToken(descriptionMultipart, imageMultiPart, token)
+
+            assertEquals(BaseResponse(error = true, actual.message), actual)
+
+            verify(storyApiService).addNewStoryWithToken(
+                descriptionMultipart,
+                imageMultiPart,
+                token
+            )
+        }
+
+    @Test
+    fun `given BaseResponse(error = true) addNewStoryWithToken should return BaseResponse(error = true)`() =
+        runBlockingTest {
+            `when`(
+                storyApiService.addNewStoryWithToken(
+                    descriptionMultipart,
+                    imageMultiPart,
+                    token
+                )
+            ).thenReturn(
+                BaseResponse(error = true)
+            )
+
+            val actual =
+                storyRepository.addNewStoryWithToken(descriptionMultipart, imageMultiPart, token)
+
+            assertEquals(BaseResponse(error = true), actual)
+
+            verify(storyApiService).addNewStoryWithToken(
+                descriptionMultipart,
+                imageMultiPart,
+                token
+            )
+        }
+
+    @Test
+    fun `given exception getStoriesWithTokenAndLocation should return PageResponse(error = true)`() =
+        runBlockingTest {
+            `when`(storyApiService.getStoriesWithTokenAndLocation(token = "Bearer $token")).thenThrow(
+                IllegalArgumentException()
+            )
+
+            val actual = storyRepository.getStoriesWithTokenAndLocation(token)
+
+            assertEquals(
+                PageResponse<StoryResponse>(error = true, emptyList(), actual.message),
+                actual
+            )
+
+            verify(storyApiService).getStoriesWithTokenAndLocation(token = "Bearer $token")
         }
 }
