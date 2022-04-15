94a8a93e5624d26ce21c9293e279a17356d143c1
onirutlA <alturino001@gmail.com>
2022-04-15 11:25:44 +0700

    refactor: remove unused comment in MapStoryActivity.kt

--- app/src/main/java/com/onirutla/storyapp/ui/map/MapStoryActivity.kt
+++ app/src/main/java/com/onirutla/storyapp/ui/map/MapStoryActivity.kt
@@ -29,7 +29,6 @@ class MapStoryActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapStoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -64,19 +63,9 @@ class MapStoryActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
         mMap.uiSettings.isZoomControlsEnabled = true
         mMap.uiSettings.isIndoorLevelPickerEnabled = true
         mMap.uiSettings.isCompassEnabled = true
