014b24cfafbe91a5e7c1aea248c68b99c0488e3c
onirutlA <alturino001@gmail.com>
2022-04-15 11:26:40 +0700

    refactor: remove File import in StoryRepository.kt

--- app/src/main/java/com/onirutla/storyapp/data/source/repository/story/StoryRepository.kt
+++ app/src/main/java/com/onirutla/storyapp/data/source/repository/story/StoryRepository.kt
@@ -5,7 +5,6 @@ import com.onirutla.storyapp.data.model.BaseResponse
 import com.onirutla.storyapp.data.model.story.StoryResponse
 import kotlinx.coroutines.flow.Flow
 import okhttp3.MultipartBody
-import java.io.File
 
 interface StoryRepository {
 
